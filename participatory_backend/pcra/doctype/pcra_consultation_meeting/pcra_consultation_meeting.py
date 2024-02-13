b776040603e9bee8cd02877a14c8e83d819a267d
nyaga <Steve Nyaga>
2024-02-13 03:54:45 +0300

    refactor: Refactor doctype definitions for PCRA

---
+++ participatory_backend/pcra/doctype/pcra_consultation_meeting/pcra_consultation_meeting.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Steve Nyaga and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+
+class PCRAConsultationMeeting(Document):
+	pass
